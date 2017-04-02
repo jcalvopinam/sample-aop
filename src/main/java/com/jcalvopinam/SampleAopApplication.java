a650efa0007eca00c162e1c54c716a7bd7b72677
Juan Calvopina <juan.calvopina+dev@gmail.com>
2017-04-02 17:32:28 -0300

    Project renamed

--- src/main/java/com/jcalvopinam/SampleAopApplication.java
+++ src/main/java/com/jcalvopinam/SampleAopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PocAopApplication {
+public class SampleAopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PocAopApplication.class, args);
+        SpringApplication.run(SampleAopApplication.class, args);
     }
 
 }
--- src/test/java/com/jcalvopinam/PocAopApplicationTests.java
+++ src/test/java/com/jcalvopinam/PocAopApplicationTests.java
@@ -1,16 +1,34 @@
 package com.jcalvopinam;
 
+import com.jcalvopinam.service.PersonService;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.rule.OutputCapture;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author juanca <juan.calvopina+dev@gmail.com>
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PocAopApplicationTests {
+public class SampleAopApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    PersonService personServiceMock;
+
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
+
+    @Test
+    public void testDefaultSettings() throws Exception {
+        personServiceMock.setName("Juanca");
+        String output = this.outputCapture.toString();
+        assertThat(output).contains("Juanca");
+    }
 
 }
